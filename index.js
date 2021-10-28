3380e2df803ca23712e020eaa426e1b503c4823b
TboOffical <77156339+TboOffical@users.noreply.github.com>
2021-10-27 20:35:18 -0400

    First Commit
    Yay

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.IsWord = function(word)
+{
+    //Check for words
+    if(word == "this") return true;
+    if(word == "lol") return false;
+    if(word == "1") return false;
+    if (word == dog) return true;
+}
